4d7d0e5377b3587b4f6d08c11271c35c3099e243
Gerold Pummer <gerold.pummer@heliumv.com>
2013-09-13 16:38:34 +0200

    PJ18128: null-Properties nicht mehr ausgeben

--- src/com/heliumv/tools/HeliumJsonProvider.java
+++ src/com/heliumv/tools/HeliumJsonProvider.java
@@ -11,6 +11,7 @@ public class HeliumJsonProvider extends JacksonJaxbJsonProvider {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(org.codehaus.jackson.map.DeserializationConfig.Feature.UNWRAP_ROOT_VALUE, false);
         mapper.configure(org.codehaus.jackson.map.SerializationConfig.Feature.WRAP_ROOT_VALUE, false);
+        mapper.configure(org.codehaus.jackson.map.SerializationConfig.Feature.WRITE_NULL_PROPERTIES, false) ;
 
         SimpleModule testModule = new SimpleModule("MyModule", new Version(1, 0, 0, null))  ;
         testModule.addSerializer(String.class, new HvStringSerializer()) ;
